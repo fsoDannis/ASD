4024813db83131d8fb527b675feb597d8c36498d
Dan <Dan@Dinkydetails.com>
2011-11-03 20:47:37 -0400

    Not sure about this CSV stuff!

--- annis_dan_project2/main.js
+++ annis_dan_project2/main.js
@@ -36,4 +36,69 @@ $.getJSON('taskList.json', function(data) {
 
 
 
-	
+//Load CSV Data
+$(document).ready(function() { 
+ 
+ 
+    $.ajax({ 
+       type: 'GET', 
+       url: 'supDataPHP.php', 
+       data: null, 
+       success: function(text) { 
+        
+        
+           var fields = text.split(/\n/); 
+           fields.pop(fields.length-1); 
+            
+            
+           var headers = fields[0].split(','), html = '<table>'; 
+            
+           html += '<tr>'; 
+            
+           for(var i = 0; i < headers.length; i += 1) { 
+            
+              html += '<th scope="col">' + headers[i] + '</th>'; 
+               
+           } 
+            
+           html += '</tr>'; 
+            
+           var data = fields.slice(1, fields.length); 
+            
+            
+            
+           for(var j = 0; j < data.length; j += 1) { 
+            
+            
+            
+              var dataFields = data[j].split(','); 
+               
+              html += '<tr>'; 
+              html += '<td>' + dataFields[0] + '</td>'; 
+              html += '<td><a href="' + dataFields[1] + '">' + dataFields[1] + '</a></td>'; 
+              html += '<td>' + dataFields[2] + '</td>'; 
+              html += '</tr>'; 
+               
+            
+            
+           } 
+            
+           html += '</table>'; 
+            
+            
+           $(html).appendTo('#supContact'); 
+            
+            
+            
+        
+        
+        
+       } 
+     
+     
+     
+    }); 
+ 
+ 
+ 
+});
